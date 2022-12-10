aaa69ccbd620abf4c9e7d2068efd87cfa48d7895
Scott Finneran <scottfinneran@yahoo.com.au>
2022-12-10 14:01:56 +1100

    Add countdown to begin. Backing track. Spacebar to pause. q to quit

--- reaper.py
+++ reaper.py
@@ -21,22 +21,24 @@ done = False
 clock = pygame.time.Clock()
 
 font = pygame.font.Font(None, 75)
+big_font = pygame.font.Font(None, 400)
 
 frame_count = 0
 frame_rate = 60
 start_time = 90
 
-# -------- Main Program Loop -----------
-while not done:
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.QUIT:  # If user clicked close
-            done = True  # Flag that we are done so we exit this loop
+state = 1
 
-    # Set the screen background
-    screen.fill(BACKGROUND)
+running = True
 
-    # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
+# Provide background noise
+background_sound = './party.ogg'
+pygame.mixer.init()
+pygame.mixer.music.load(background_sound)
 
+
+def timer():
+    print("tick")
     # --- Timer going up ---
     # Calculate total seconds
     total_seconds = frame_count // frame_rate
@@ -66,8 +68,51 @@ while not done:
 
     screen.blit(text, [50, 300])
 
+def countdown(seconds):
+    output_string = "{0:2}".format(seconds)
+    text = big_font.render(output_string, True, FOREGROUND)
+    screen.blit(text, [200,150])
+
+
+# Main Program Loop
+while not done:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q: # Quit
+                done = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE: # toggle pause
+                running = not running
+                if running:
+                    pygame.mixer.music.play()
+                else:
+                    pygame.mixer.music.pause()
+
+    # Set the screen background
+    screen.fill(BACKGROUND)
+
+    # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
+    if state == 1: # Initial count down to begin
+        seconds_until_start = 10 - (frame_count // frame_rate)
+
+        countdown(seconds_until_start)
+        if seconds_until_start <= 0:
+            frame_count = 0
+            state += 1
+        seconds_until_start -= seconds_until_start
+
+    elif state == 2: # Prep for main run
+        pygame.mixer.music.play()
+        state += 1
+
+    elif state == 3: # Main run
+        timer()
+
     # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
-    frame_count += 1
+    if running:
+        frame_count += 1
 
     # Limit frames per second
     clock.tick(frame_rate)
@@ -75,6 +120,4 @@ while not done:
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
 pygame.quit()
